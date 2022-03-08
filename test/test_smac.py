3027996a25713deb4c6ae18fa129069edc90b3e4
J. Rook <jrook94@gmail.com>
2022-03-08 16:37:24 +0100

    Extended base test

--- test/test_smac.py
+++ test/test_smac.py
@@ -6,7 +6,9 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import pcsparser
 
 
-def test_SMACparse():
+def test_parse():
     parser = pcsparser.PCSParser()
     parser.load("files/smacpcs.pcs")
     assert parser.check_validity() is True
+
+    parser.export("paramils", "/dev/null")
